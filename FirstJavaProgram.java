6954b8afc8a46675587c8a53bed8147044dadf6d
Your Name Dorinetk <dketchiozo@gmail.com>
2020-07-12 21:35:05 -0400

    testing arguments

--- FirstJavaProgram.java
+++ FirstJavaProgram.java
@@ -1,5 +1,7 @@
 public class FirstJavaProgram {
-    public static void main (String[] args){
+    public static void main(String[] args) {
+        System.out.println(args[0] +"." + args[1] + " Both are friends");
         System.out.println("Hello World");
     }
+
 }
