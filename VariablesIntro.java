1be25c052d5d26aa0c8912be60909292c9ee18fd
Your Name Dorinetk <dketchiozo@gmail.com>
2020-07-14 21:58:22 -0400

    introduction to variables

---
+++ VariablesIntro.java
@@ -0,0 +1,10 @@
+
+public class VariablesIntro {
+
+    public static void main(String[] args) {
+        int age = 42;
+        char nameFirstLetter = 'B';
+        String name= "Beatrice";
+        System.out.println(name + " starts with " + nameFirstLetter + ", and she is " + age + " years old.");
+    }
+}
