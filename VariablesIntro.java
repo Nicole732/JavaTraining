51ee96b744db5635dc98d6433666ad48ac2b8168
Your Name Dorinetk <dketchiozo@gmail.com>
2020-07-18 00:42:44 -0400

    addition of numbers with user's input

--- VariablesIntro.java
+++ VariablesIntro.java
@@ -1,11 +1,37 @@
+import java.util.Scanner;
 
 public class VariablesIntro {
 
+    static String name = "Beatrice";
+
+    public void totalIncome(int[] args) {
+
+    }
+
     public static void main(String[] args) {
+        // use of different variable types
         int age = 42;
         char nameFirstLetter = 'B';
-        String name= "Beatrice";
+        System.out.println("###############");
         System.out.println(name + " starts with " + nameFirstLetter + ", and she is " + age + " years old.");
+        System.out.println("###############");
+
+        // addition of two numbers using the Scanner utility
+        int salary1, salary2, totalSalary;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the first salary: ");
+        salary1 = sc.nextInt();
+        System.out.println("Enter the second salary: ");
+        salary2 = sc.nextInt();
+
+        sc.close();
+        totalSalary = salary1 + salary2;
+        System.out.print(name + "'s annual income is: ");
+        System.out.print("$" + totalSalary + "\n");
+        System.out.println("###############");
+
+        // addition using a method with arguments input??
+
     }
 
 }
