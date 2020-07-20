bee0180e0f8097c7c5a964d6a1ad7664724eef90
Your Name Dorinetk <dketchiozo@gmail.com>
2020-07-19 21:45:10 -0400

    added another way to add arguments from user

--- VariablesIntro.java
+++ VariablesIntro.java
@@ -4,14 +4,16 @@ public class VariablesIntro {
 
     static String name = "Beatrice";
 
-    /*
-     * public void totalIncome(int[] args) {
-     * 
-     * 
-     * }
-     */
+    public static void totalIncome(String[] args) {
+        int salary1Arg = Integer.parseInt(args[0]);
+        int salary2Arg = Integer.parseInt(args[1]);
+        int totalSalaryArg = salary1Arg + salary2Arg;
+        System.out.println(name + " annual salary with the provided arguments is " + totalSalaryArg);
+
+    }
 
     public static void main(String[] args) {
+
         // use of different variable types
         int age = 42;
         char nameFirstLetter = 'B';
@@ -32,7 +34,12 @@ public class VariablesIntro {
         System.out.print("$" + totalSalary + "\n");
         System.out.println("###############");
 
-        // addition using a method totalIncome with int arguments input??
+        // addition using a method totalIncome with int arguments input
+        //totalIncome();
+        int salary1Arg = Integer.parseInt(args[0]);
+        int salary2Arg = Integer.parseInt(args[1]);
+        int totalSalaryArg = salary1Arg + salary2Arg;
+        System.out.println(name + " annual salary with the provided arguments is " + totalSalaryArg);
 
     }
 
