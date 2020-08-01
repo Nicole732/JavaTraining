a594b1e20165452d6dab475ae9787e3cbe96a6c9
Your Name Dorinetk <dketchiozo@gmail.com>
2020-08-01 17:20:00 -0400

    uses char type for the operator in calculator

--- CalculatorWithSwitch.java
+++ CalculatorWithSwitch.java
@@ -2,18 +2,20 @@ public class CalculatorWithSwitch {
     public static void main(String[] args) {
         int num1 = Integer.parseInt(args[0]);
         int num2 = Integer.parseInt(args[1]);
-        String operator = args[2];
+        char operator = args[2].charAt(0);
+        //need to provide the * operator as '*' when giving the argument. * is reserved in Java
+        //could also use String operator = args[2], and change cases to "+"... not best practice
         switch (operator) {
-            case "+":
+            case '+':
                 System.out.println("The sum of the two numbers is: " + (num1 + num2));
                 break;
-            case "-":
+            case '-':
                 System.out.println("The substraction of the two numbers gives: " + (num1 - num2));
                 break;
-            case "/":
+            case '/':
                 System.out.println("The division of the two numbers gives: " + (num1 / num2));
                 break;
-            case "*":
+            case '*':
                 System.out.println("The product of the two numbers gives: " + (num1 * num2));
                 break;
             default:
