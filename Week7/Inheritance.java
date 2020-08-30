fa773dcfce671ea7a366708c1d5b1695aa771c7c
Your Name Dorinetk <dketchiozo@gmail.com>
2020-08-29 20:09:12 -0400

    week8: polymorphism concepts overriding and overloading

--- Week7/Inheritance.java
+++ Week7/Inheritance.java
@@ -8,7 +8,6 @@ class Bicycle {
         this.gear = gear;
         this.speed = speed;
     }
-    
 
     public void applyBrake(int decrement) {
         speed -= decrement;
@@ -19,6 +18,7 @@ class Bicycle {
     }
 
 }
+
 class MountainBike extends Bicycle {
     /*
      * the MountainBike subclass adds one more field *int seatHeight*. the
@@ -31,22 +31,39 @@ class MountainBike extends Bicycle {
     int startHeight;
 
     // Bicycle mountainBicycle;
-     MountainBike(int gear, int speed, int startheight) {
-      super(gear,speed);
-    // this.startHeight = startHeight;
-    // }
+    MountainBike(int gear, int speed, int startHeight) {
+        super(gear, speed);
+        this.startHeight = startHeight;
+    }
+
     public void setHeight(int newValue) {
         // this.newValue = newValue;
     }
 
 }
 
+/* 
+Create a class *Owner* with fields *String name and int age* and make sure the  MountainBike HAS-A owner.
+*/
+class Owner {
+    
+}
+
+
+/* 
+Write a *Test*  class where you will be creating a MountainBike object and print the properties of that object
+*/
+class Test {
+    
+       
+}
+
 public class Inheritance {
     public static void main(String[] args) {
         System.out.println("Main Method");
         Bicycle joelBike = new Bicycle(2,3);
-        joelBike
+      
         
     }
-    
+
 }
---
+++ week8.java
@@ -0,0 +1,48 @@
+/* *Lab* : Write a superclass Animal that has a method called animalSound() 
+which print message "The animal makes a sound".
+ Write a Subclasses of Animals Pigs, Cats, and their own implementation of 
+ an animalSound() which print the "pig oinks" for Pigs and the "cat meows" for Cats. 
+ Add additional animalSound(int num) method in Cats class that print the number of time the cat meows.
+
+In a Test class, create an Animal Pig and Cat  objects and 
+call the animalSound() method on them, 
+also call the animalSound(2) on Cat object
+ */
+
+class Animal {
+    public void animalSound() {
+        System.out.println("The animal makes a sound");
+    }
+
+}
+class Pigs extends Animal{
+    @Override
+    public void animalSound(){
+        System.out.println("pig oinks");
+    }
+
+}
+class Cats extends Animal{
+    @Override
+    public void animalSound(){
+        System.out.println("cat meows");
+    }
+
+    
+    public void animalSound(int num){
+        System.out.println("The cat meows " + num + " times.");
+    }
+}
+
+public class week8 {
+
+    public static void main(String[] args) {
+     Pigs TonyPig = new Pigs();
+     TonyPig.animalSound();
+
+     Cats Cathy = new Cats();
+     Cathy.animalSound();
+     Cathy.animalSound(10);
+        
+    }
+}
