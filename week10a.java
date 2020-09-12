e403a0d5f602001f01c84d8466a19bd709f8328c
Your Name Dorinetk <dketchiozo@gmail.com>
2020-09-12 18:06:25 -0400

    reversed the word

--- week10a.java
+++ week10a.java
@@ -4,10 +4,12 @@ A palindrome is a word, phrase, number, or other sequence of characters which re
 */
 import java.util.Scanner;
 
-class reverse {
-    public String reverseWord(String str){
-        return str;
+class reverseWord {
+    public void reverseWord(String str){
+        String[] testWord = str.split(" ");
+        System.out.println(testWord);
 
+       
     }
 
 
@@ -16,18 +18,25 @@ class reverse {
 public class week10a {
 
     public static void main(String[] args) {
-        //receive input from user
+        //receive input from user, trims and convert it to lower case
         System.out.println("Enter a word and let us see if it is a palindrome! ");
         Scanner tw = new Scanner(System.in);
         String testWord = new String(tw.nextLine());
         tw.close();
+        testWord = testWord.toLowerCase().trim();
         System.out.println(testWord);
+        
         //reverse the word
+        String reversedWord = "";
+        char[] ch = testWord.toCharArray();
+        for (int i = ch.length - 1; i >= 0; i--) {
+            reversedWord += ch[i];
+        }
+        System.out.println("Reverse of provided word is: " + reversedWord.toLowerCase().trim());
+        
+        //Compare the two word to see if they are equal 
+        //then conclude that it is a palindrome
+        
 
     }
-   
-
-
-
-    
 }
