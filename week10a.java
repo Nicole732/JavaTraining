e39bb15b6721d76340269971f2b73c43930d639c
Your Name Dorinetk <dketchiozo@gmail.com>
2020-09-12 17:22:55 -0400

    used stored the input word as a String object instead

--- week10a.java
+++ week10a.java
@@ -4,16 +4,26 @@ A palindrome is a word, phrase, number, or other sequence of characters which re
 */
 import java.util.Scanner;
 
+class reverse {
+    public String reverseWord(String str){
+        return str;
+
+    }
+
+
+}
+
 public class week10a {
 
     public static void main(String[] args) {
         //receive input from user
-        String testWord;
         System.out.println("Enter a word and let us see if it is a palindrome! ");
         Scanner tw = new Scanner(System.in);
-        testWord = tw.nextLine();
+        String testWord = new String(tw.nextLine());
         tw.close();
         System.out.println(testWord);
+        //reverse the word
+
     }
    
 
